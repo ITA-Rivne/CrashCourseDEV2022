029121ccd3ad4c61777614bfee1d970f9634fe02
Anton <romanukhaav@gmail.com>
2022-08-08 20:59:48 +0300

    Test commit from VS

---
+++ Projects/RomanukhaAV/myFirstProject/myFirstProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace myFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+
+        }
+    }
+}
