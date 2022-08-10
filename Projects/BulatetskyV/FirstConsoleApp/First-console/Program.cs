c4de1c27e828066d7ec3a55f2cbc762e59a85416
Degreener <105043021+NomadCatB@users.noreply.github.com>
2022-08-10 10:37:00 +0300

    First Console Apps

---
+++ Projects/BulatetskyV/FirstConsoleApp/First-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace First_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
+        }
+    }
+}
