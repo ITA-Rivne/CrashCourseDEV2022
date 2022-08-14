b550ebcbdec04fa73167fe624a4b45266701ba9e
OlenaSobko <olenasobko.ua@gmail.com>
2022-08-14 17:20:08 +0300

    add some homework

---
+++ Projects/Examples/202/202/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _202
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To be or not to be");
+            Console.WriteLine(@"\Shakespeare\");
+            Console.ReadLine();
+        }
+    }
+}
