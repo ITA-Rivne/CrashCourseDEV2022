30e6ebf87ec6307f42ce3d0f5ba25a0f2cfed56b
Degreener <105043021+NomadCatB@users.noreply.github.com>
2022-08-15 16:48:04 +0300

    New Simple Progect

---
+++ Projects/BulatetskyV/SimpleTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Simple Tasks
+            
+            //
+        }
+    }
+}
