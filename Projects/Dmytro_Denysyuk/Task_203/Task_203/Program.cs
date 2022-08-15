07db34ab00a022fe0b94846475745cf7bb2f0c0f
WebDev-Den <73542468+WebDev-Den@users.noreply.github.com>
2022-08-15 12:32:29 +0300

    Dmytro Denysyuk Task
    Ready work Task 203 Lesso 3

---
+++ Projects/Dmytro_Denysyuk/Task_203/Task_203/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Task_203
+{
+    static void Main()
+    {
+
+        while (true)
+        {
+            int number;
+
+            try
+            {
+                Console.WriteLine("Input Number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Result: " + (number % 2 == 0 ? "pair" : "odd"));
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+        }
+    }
+}
