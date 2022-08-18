5658e16c8bcc0a14e1a2d5aac1bc5b8b5b6c95b6
WebDev-Den <73542468+WebDev-Den@users.noreply.github.com>
2022-08-18 19:44:32 +0300

    Add ready task 204-206
    Read ytask from Dmytro Denysyuk

---
+++ Projects/Dmytro_Denysyuk/Task_204/Task_204/Program.cs
@@ -0,0 +1,24 @@
+﻿int number,sum;
+while (true)
+{ 
+    sum = 0;
+    try
+    {
+        Console.WriteLine("Input number a<100:");
+        number = Convert.ToInt32(Console.ReadLine());
+       if(number<100 && number > 0)
+        {
+            char[] charArr = number.ToString().ToCharArray(); 
+            foreach (char ch in charArr)
+                sum+=Convert.ToInt32(ch.ToString());
+            Console.WriteLine("Size: " + charArr.Length);
+            Console.WriteLine("Sum: " + sum);
+        }
+        else
+            Console.WriteLine("Error: Size numbert not correct");
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine("Error: " + e.Message);
+    }
+}
---
+++ Projects/Dmytro_Denysyuk/Task_205/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Task_205
+{
+    static void Main()
+    {
+        int number;
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Input number:");
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.Write(number > 0 ? "-" : "");
+                number = System.Math.Abs(number);
+                char[] charArr = number.ToString().ToCharArray();
+                Array.Reverse(charArr);
+                foreach (char ch in charArr)
+                    Console.Write(ch);
+                Console.WriteLine();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+        }
+    }
+}
