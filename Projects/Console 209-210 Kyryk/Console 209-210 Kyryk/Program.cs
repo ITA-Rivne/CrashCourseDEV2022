44dc57e6fb771ce463fc8dc0bc9fc625896960d0
Tetyana-K <tanya.kyryk@gmail.com>
2022-08-18 23:54:11 +0300

    created new project and started task 209

---
+++ Projects/Console 209-210 Kyryk/Console 209-210 Kyryk/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Console_209_210_Kyryk
+{
+    class Client
+    {
+        //Написати overload методи Add які в залежності від параметрів або додають цифрові значення, або об'єднують введені тексти.
+        //Передбачити аргументи за замовчуванням і використання статичних полів\методів
+        public static void Task_209()
+        {
+
+        }
+        public static void Task_210()
+        { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
