d780dc77dcb7b87aa955b34612e4248e6427a533
smoljankin <smoljankin@gmail.com>
2022-08-18 13:08:38 +0300

    Definition of an even number

---
+++ Projects/Smoljankin/Task_203/Task_203/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x; ;
+            try
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                if(x % 2 == 0)
+                {
+                    Console.WriteLine("Number is even");
+                }
+                else
+                {
+                    Console.WriteLine("Number is not even");
+                }
+                
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid input format");
+            }
+
+        }
+    }
+}
