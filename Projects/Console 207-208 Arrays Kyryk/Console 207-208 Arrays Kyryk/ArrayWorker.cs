4bd3a56dda0ef4c242d7a7dbb4d8bbdab4eefc46
Tetyana-K <tanya.kyryk@gmail.com>
2022-08-18 22:43:45 +0300

    create project and class ArrayWorker

---
+++ Projects/Console 207-208 Arrays Kyryk/Console 207-208 Arrays Kyryk/ArrayWorker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_207_208_Arrays_Kyryk
+{
+    static class ArrayWorker
+    {
+        public static void Random(int [] arr, int left = 0, int right = 100)
+        {
+             Random rnd = new Random();
+            if (left > right)
+                Swap(ref left, ref right);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(left, right);
+            }
+            
+        }
+
+        private static void Swap(ref int left, ref int right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Projects/Console 207-208 Arrays Kyryk/Console 207-208 Arrays Kyryk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_207_208_Arrays_Kyryk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
