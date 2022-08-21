0c9cea5906cebedaad7eaa8011a5d93afe4c3202
Bulatetska <110628900+Bulatetska@users.noreply.github.com>
2022-08-21 21:22:16 +0300

    Update Animal.cs

--- Projects/Team2/Сheburashka_Project/Cheburashka/Animal.cs
+++ Projects/Team2/Сheburashka_Project/Cheburashka/Animal.cs
@@ -8,46 +8,16 @@ namespace Cheburashka
 {
     class Animal : IPerson
     {
-        private string _name;
-        public int weight;
-        public static Random random = new Random();
-
-        public Animal(string name, int weight)
-        {
-            this._name = name;
-            this.weight = weight;
-        }
-        public Animal(string name)
-        {
-            this._name = name;
-            this.weight = random.Next(1, 10);
-        }
-
-
-        public string Name
-        {
-            get => _name;
-            set => _name = value;
-        }
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public void GetWork(string work, int count)
         {
-            if (count <= weight)
-            {
-                string a = Say() + $"Я виконую роботу {work} і несу цеглу кількістю {count}";
-                Console.WriteLine(a);
-            }
-            else
-                Console.WriteLine(Say() + $"Я не можу підняти цеглин кількістю {count}, максимум можу підняти кількість цеглин {weight} ");
-
+            throw new NotImplementedException();
         }
 
         public string Say()
         {
-            string s = $"Привіт! Я {this._name}!";
-
-            return s;
-
+            throw new NotImplementedException();
         }
     }
 }
