cae5e12f8aad19683cdde5c6d2c8a3a72a8f788e
smoljankin <smoljankin@gmail.com>
2022-08-21 18:31:48 +0300

    The minimum odd number in the array

---
+++ Projects/Smoljankin/Task_208/Task_208/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task_208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 100;
+
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            int[] arr = new int[10];
+           
+            int min = N;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(N);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] % 2 == 1)
+                {
+                    min = arr[i];
+                    break;
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 1)
+                {
+                    if(arr[i] < min)
+                    min = arr[i];
+                   
+                }
+            }
+
+            Console.WriteLine("Min = " + min);
+        }
+    }
+}
