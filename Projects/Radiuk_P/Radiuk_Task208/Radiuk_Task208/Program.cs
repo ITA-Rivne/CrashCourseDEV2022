0fab1b71ae26aefc79ad765a69a2cb2d0b389588
Pavlo Radiuk <radiukpavlo@gmail.com>
2022-08-21 14:03:24 +0300

    [radiuk_vs2] Update task208
    In this update, the program checks whether an input array contains at least one odd number: if yes, it finds the smallest number, else it throws a message.

--- Projects/Radiuk_P/Radiuk_Task208/Radiuk_Task208/Program.cs
+++ Projects/Radiuk_P/Radiuk_Task208/Radiuk_Task208/Program.cs
@@ -8,31 +8,44 @@ namespace Radiuk_Task208
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-			int[] arrayInput = { -10, -9, 2, 1, -5, 0, 10, 7 };
+        static void Main()
+        {		
+			// Define the input string
+			Console.WriteLine("Enter the array of integers with ',' as a seperator:");
+			string foo = Console.ReadLine();
+			string[] tokens = foo.Split(',');
 
-			int index = 0;
-			while (arrayInput[index] % 2 == 0)
+			// Define the array of integers
+			List<int> arrayToCheck = new List<int>();
+
+			// Add the entered from a keyboard integers to the array
+			int numberToAdd;
+
+            foreach (string s in tokens)
 			{
-				index++;
-			}
+				if (int.TryParse(s, out numberToAdd))
+                {
+					arrayToCheck.Add(numberToAdd);
+                }
+            }
 
-			int numberMinOdd = arrayInput[index];
+			// Check whether at least one element is odd
+			bool allAreEven = !arrayToCheck.Any(num => (num % 2) != 0);
 
-			while (index < arrayInput.Length)
+			// Initialize the output value
+            int numberMinOdd = 0;
+
+			// If at least one element is odd, find the smallest one
+			// Else throw an exception
+            if (allAreEven == false)
 			{
-				if (arrayInput[index] % 2 != 0)
-				{
-					if (arrayInput[index] < numberMinOdd)
-					{
-						numberMinOdd = arrayInput[index];
-					}
-				}
-				index++;
-			}
-
-			Console.WriteLine("The smallest odd number is {0}.", numberMinOdd);
+                numberMinOdd = arrayToCheck.Where(num => (num % 2) != 0).Min();
+                Console.WriteLine($"The smallest odd number is {numberMinOdd}.");
+            }
+			else
+			{
+                Console.WriteLine($"No odd numbers found.");
+            }
 			Console.ReadLine();
 		}
     }
