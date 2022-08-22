a28a40315d386717c4aebfdc1d4d293ca45fabef
Degreener <105043021+NomadCatB@users.noreply.github.com>
2022-08-22 13:16:59 +0300

    Add tasks 205-206

--- Projects/BulatetskyV/SimpleTask/Program.cs
+++ Projects/BulatetskyV/SimpleTask/Program.cs
@@ -12,8 +12,8 @@ namespace SimpleTask
         {
             //Simple Tasks
 
-            //Average of two numerics
-            Console.WriteLine("-------Average of 2 numerics-------");
+            //201. Average of two numerics
+            Console.WriteLine("-------201. Average of 2 numerics-------");
             Console.WriteLine("Input two numerics");
             float a1 = Convert.ToSingle (Console.ReadLine());
             float a2 = Convert.ToSingle (Console.ReadLine());
@@ -21,21 +21,21 @@ namespace SimpleTask
             Console.WriteLine("Average of " + a1 + " and "+ a2 + " is ="+ aver);
             Console.ReadKey();
 
-            //Text from Shakespeare
-            Console.WriteLine("-------Text from Shakespeare-------");
+            //202. Text from Shakespeare
+            Console.WriteLine("-------202. Text from Shakespeare-------");
             Console.WriteLine("To be or not to be \n\\ Shakespeare \\");
             Console.ReadKey();
 
-            //Check for parity
-            Console.WriteLine("-------Check for Parity-------");
+            //203. Check for parity
+            Console.WriteLine("-------203. Check for Parity-------");
             Console.WriteLine("Input numeric:");
             int a3 = Convert.ToInt32(Console.ReadLine());
             if (a3 % 2 == 0) Console.WriteLine("Pair");
             else Console.WriteLine("Odd");
             Console.ReadKey();
 
-            //Quantity of digits and its sum of numeric from 0 to 99
-            Console.WriteLine("-------Quantity of digits and its sum of numeric from 0 to 99-------");
+            //204. Quantity of digits and its sum of numeric from 0 to 99
+            Console.WriteLine("-------204. Quantity of digits and its sum of numeric from 0 to 99-------");
             Console.WriteLine("Input numeric:");
             int a4 = Convert.ToInt32(Console.ReadLine());
             if (a4 < 100)
@@ -46,9 +46,39 @@ namespace SimpleTask
             else Console.WriteLine("So big numeric!");
             Console.ReadKey();
 
+            //205. Revert numeric
+            Console.WriteLine("-------205. Revert numeric-------");
+            Console.WriteLine("Input numeric:");
+            string a5 = Console.ReadLine();    
+            long n5 = Convert.ToInt32(a5);
+            for (int i = 0; i < a5.Length; i++)
+            {
+                long rs5 = n5 % 10;
+                if (rs5 != 0)
+                    Console.Write(rs5);
+                n5 /= 10;
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+            //206. Sum of digits of numeric
+            Console.WriteLine("-------206. Sum of digits of numeric-------");
+            Console.WriteLine("Input numeric:");
+            string a6 = Console.ReadLine();
+            long n6 = Convert.ToInt32(a6);
+            long s6 = 0;
+            for (int i = 0; i < a6.Length; i++)
+            {
+                long rs6 = n6 % 10;
+                if (rs6 != 0)
+                n6 /= 10;
+                s6 = s6+ rs6;
+            }
+            Console.WriteLine(s6);
+            Console.ReadKey();
+
             //Soon to be continued
-        
-        
+
         }
     }
 }
