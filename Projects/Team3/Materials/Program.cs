eb689d657d99f1a653e6dc5eff86fd19064aeef8
WebDev-Den <73542468+WebDev-Den@users.noreply.github.com>
2022-08-22 18:08:48 +0300

    Add class for team 3
    Add class for team 3 from Dmytro Denysyuk

---
+++ Projects/Team3/Materials/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+namespace Materials
+{ 
+    public class Materials
+    {
+        private float width, height, length,rotate;
+        private string type;
+        public Materials(string type,float width, float height, float length )
+        {
+            this.type = type;
+            this.width = width;
+            this.length = length;
+            this.height = height;
+            this.rotate = 0;
+        }
+        public void SetRotate(float rotate)
+        {
+            this.rotate = rotate;
+
+        }
+        public float GetPerimeter()
+        {
+            return (this.width + this.length)*2;
+        }
+        public float GetArea()
+        {
+            return this.width * this.length;
+
+        }
+        public float GetVolume()
+        {
+            return this.GetArea()* this.height;
+        }
+
+
+
+    }
+}
