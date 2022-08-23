74f380e66b36b7272b39cd6d5ed7be32da1c8412
Tetyana-K <tanya.kyryk@gmail.com>
2022-08-23 22:00:35 +0300

    Create demo Main()

---
+++ Projects/Team3/Smart_House/Smart_House/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smart_House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Smart House!");
+        }
+    }
+}
