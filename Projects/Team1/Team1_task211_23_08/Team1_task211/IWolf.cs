665e8eed6bc6c149ace57afe2661c81c01c484f4
momolchanova <110627678+momolchanova@users.noreply.github.com>
2022-08-23 22:49:34 +0300

    Add files via upload

---
+++ Projects/Team1/Team1_task211_23_08/Team1_task211/IWolf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1_task211
+{
+    interface IWolf
+    {
+        void findPiggi();
+        int distraction(int power);
+        void eat();
+    }
+}
