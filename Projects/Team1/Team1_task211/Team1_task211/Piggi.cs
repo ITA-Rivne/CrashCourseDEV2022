da728b7b165d026195f562ce5595c9f929c0b94f
momolchanova <110627678+momolchanova@users.noreply.github.com>
2022-08-24 11:12:16 +0300

    Додано інтерфейси і клас поросятка

---
+++ Projects/Team1/Team1_task211/Team1_task211/Piggi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1_task211
+{
+    class Piggi : IAnimal,IPiggi
+    {
+        String name;
+        int smart; // рівень розуму поросятка (від 1 до 10)
+        int power; // рівень сили поросятка (від 1 до 10)
+        int speed; // рівень швидкості поросятка (від 1 до 10)
+        int distrLevel; //рівень стійкості будинку до руйнування
+        bool died = false;
+        bool vtik = false;
+        int BaseDistance;
+
+        public Piggi(String name)
+        {
+            this.name = name;
+            this.smart = new Random().Next(1, 10);
+            this.power = new Random().Next(1, 10);
+            this.speed = new Random().Next(1, 10);
+
+            Console.WriteLine("Я " +  name + " Я сильний(-a) з оцінкою "+ power);
+            Console.ReadKey();
+
+        }
+
+        public void buildHouse()
+        {
+            distrLevel = (smart + power) / 2;
+            Console.WriteLine("Я побудував будинок з оцінкою стійкості" + distrLevel);
+        }
+
+
+        public void funny()
+        {
+            smart++;
+            power++;
+            speed++;
+        }
+
+        public void move(int distance)
+        {
+            distance += speed;
+            Console.WriteLine("Я пройшов ще " + speed + " метрів.");
+
+        }
+    }
+}
