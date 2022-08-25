5cd7d448ff1f11d8f55607b7f2507341e32286ed
WebDev-Den <73542468+WebDev-Den@users.noreply.github.com>
2022-08-25 10:49:07 +0300

    Create class materials
    name

---
+++ Projects/Team1/Team1_task211/Team1_task211/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1_task211
+{
+
+    internal class Materials
+    {
+        private string name;
+        private int strength; 
+
+        public Materials(string name)
+        {
+            this.name = name;
+            Console.WriteLine($"Create new materials name:{name}");
+
+        } 
+        public void SetStrength(int strength)
+        {
+            this.strength = strength;
+
+        }
+        public void GenerateStrength(int step)
+        {
+            this.strength = new Random().Next(1, 100)*step+1;
+        }
+    }
+}
