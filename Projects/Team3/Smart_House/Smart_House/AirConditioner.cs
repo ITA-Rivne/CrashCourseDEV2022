ffdb8b6c150a435ad80a058752ca345227409a52
Tetyana-K <tanya.kyryk@gmail.com>
2022-08-25 16:27:09 +0300

    create class AirConditioner

---
+++ Projects/Team3/Smart_House/Smart_House/AirConditioner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_House
+{
+    class AirConditioner : Device
+    {
+
+        private int _delta;
+        public AirConditioner(int delta = 1)
+        {
+            this._delta = delta;
+        }
+
+        public void Handler(object o, EventArgs e)
+        {
+
+            House h = o as House;
+            NeedsRegimeEventArgs ne = e as NeedsRegimeEventArgs;
+            switch (ne.NeedsRegime)
+            {
+                case NeedsRegime.NeedCool:
+                    Console.WriteLine($"\t\tNow {h.Temperature} C\u00b0 --> Air cooling");
+                    h.Temperature -= _delta;
+                    break;
+                case NeedsRegime.NeedWarm:
+                    Console.WriteLine($"\t\tNow {h.Temperature} C\u00b0 --> Air heating");
+                    h.Temperature += _delta;
+                    break;
+                default:
+                    break;
+            };
+
+
+        }
+    }
+}
