97a6be23755c302d6e6090121035ec79f7cc0d61
Tetyana-K <tanya.kyryk@gmail.com>
2022-08-25 15:58:31 +0300

    create class for my event args NeedsRegimeEventArgs

---
+++ Projects/Team3/Smart_House/Smart_House/NeedsRegimeEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_House
+{
+    enum NeedsRegime { NeedWarm, NeedCool, NeedOutFire }
+
+    class NeedsRegimeEventArgs : EventArgs
+    {
+        public NeedsRegime NeedsRegime { get; set; }
+    }
+}
