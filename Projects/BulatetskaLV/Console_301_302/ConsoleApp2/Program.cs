ec8ea265767c66da254358a4442837d92e8baaf8
Bulatetska <110628900+Bulatetska@users.noreply.github.com>
2022-08-29 18:00:40 +0300

    302
    задача 302

---
+++ Projects/BulatetskaLV/Console_301_302/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student("Semenyuk Stepan", 2, "11111111");
+
+            st.infoPrint();
+
+            Aspirant a=new Aspirant("Semenyuk Olena", 1, "1222222", "Database");
+            a.infoPrint();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Projects/BulatetskaLV/Console_301_302/ConsoleApp2/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+  public  class Student
+    {
+        public string name; // Прізвище та ім'я
+        public int course; // Курс
+        public string numberBook; // Номер залікової книжки
+        public string phone;
+
+        public Student(string name, int course, string numberBook)
+        {
+            this.name = name;
+            this.course = course;
+            this.numberBook = numberBook;
+        }
+        public Student(string name, int course, string numberBook, string phone)
+        {
+            this.name = name;
+            this.course = course;
+            this.numberBook = numberBook;
+            this.phone = phone;
+        }
+        public Student(string name, string numberBook)
+        {
+            this.name = name;
+            this.course = 1;
+            this.numberBook = numberBook;
+        }
+
+        public void infoPrint()
+        {
+           Console.WriteLine($"Name = {name}");
+            Console.WriteLine($"Course = {course}");
+            Console.WriteLine($"namberBook = {numberBook}");
+        }
+
+
+    }
+
+}
