9fa005f1ec217b919776ad80e670e61dae4804f5
BahriiR <gcardinal2009@gmail.com>
2022-08-31 16:03:31 +0300

    add Wpf project

---
+++ Projects/Team1/Team1_Wpf/Team1_WpfApp/Team1_WpfApp/Fairytale/Building.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Team1_WpfApp.Fairytale
+{
+    public class Building : IBuilding
+    {
+        public string buildingName;        
+        public bool buildingStatus = true; // whether it is intact or destroyed
+
+        private IMaterial _base;
+        private IMaterial wall;
+        private IMaterial roof;
+
+        public Building(string buildingName, IMaterial _base, IMaterial wall, IMaterial roof)
+        {
+            this.buildingName = buildingName;
+            this._base = _base;
+            this.wall = wall;
+            this.roof = roof;
+        }
+
+        public override string ToString()
+        {
+            return $"Я називаю свій будиночок '{this.buildingName}'!\n" +
+                   $"Він має міцність {this.buildingSolidity()} \n";
+        }
+
+        public int buildingSolidity()
+        {
+            return _base.Strength + wall.Strength + roof.Strength;
+        }
+
+    }
+}
---
+++ Projects/Team1/Team1_Wpf/Team1_WpfApp/Team1_WpfApp/Fairytale/Wolf.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1_WpfApp.Fairytale
+{
+    class Wolf
+    {
+        public int strenght;
+        public int speed;
+        public string name;
+
+        public Wolf(String name, int strenghtIndex = 1)
+        {
+            this.name = name;
+            Random r = new Random(strenghtIndex);
+            this.strenght = r.Next(1, 10) * strenghtIndex;
+            this.speed = r.Next(1, 10);
+
+            //Console.WriteLine("Я " + name);
+            //Console.WriteLine($"Моя сила = {this.strenght}, швидкість = {this.speed} \n");
+
+        }
+
+        public string destructionBuilding(Building house)
+        {
+            if (strenght > house.buildingSolidity())
+            {
+                house.buildingStatus = false;
+                return $"Я знищив {house.buildingName} \n";
+            }
+            else
+            {
+                return $"Будинок має міцність {house.buildingSolidity()} мені не хватає сили {strenght} \n";                
+            }
+        
+        }
+
+        public override string ToString()
+        {
+            return $"Я {this.name}  \n" +
+                   $"Моя сила = {this.strenght}, швидкість = {this.speed} \n";
+        }
+    }
+}
