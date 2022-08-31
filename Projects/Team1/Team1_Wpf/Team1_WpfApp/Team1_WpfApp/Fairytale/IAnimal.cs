2f7563bbc9821b016550c4139bc4a74da7ce5d27
momolchanova <110627678+momolchanova@users.noreply.github.com>
2022-08-31 22:11:50 +0300

    Extracted IEntity interface with "name" property. Object.ToString() changed to GetMessage() in IInformatible interface.

--- Projects/Team1/Team1_Wpf/Team1_WpfApp/Team1_WpfApp/Fairytale/IAnimal.cs
+++ Projects/Team1/Team1_Wpf/Team1_WpfApp/Team1_WpfApp/Fairytale/IAnimal.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Team1_WpfApp.Fairytale
 {
-    interface IAnimal
+    interface IAnimal : IEntity, IInformatible
     {
         void move(int distance);
     }
