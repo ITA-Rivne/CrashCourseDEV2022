d7cad14eab5f3f882a8ae7e28643e3f4be166197
TSShrol <48250029+TSShrol@users.noreply.github.com>
2022-09-12 15:03:59 +0300

    add lamp and motion device to ViewModel and xaml

--- Projects/Team3/SmartHouse_MAUI/SmartHouse_MAUI/MainPage.xaml.cs
+++ Projects/Team3/SmartHouse_MAUI/SmartHouse_MAUI/MainPage.xaml.cs
@@ -8,13 +8,26 @@ public partial class MainPage : ContentPage
     HouseViewModel houseViewModel;
     public MainPage()
 	{
-		InitializeComponent();
+      InitializeComponent();
         houseViewModel = new HouseViewModel() { Temperature = -44 };
         // houseViewModel = new HouseViewModel() ;// start temp = 20
         houseViewModel.OuterTemperature = 20;
         this.BindingContext = houseViewModel;
     }
-
+    private void BtnMonitor_Clicked(object sender, EventArgs e)
+    {
+        if (houseViewModel.OnLamp)
+        {
+            LblLamp.BackgroundColor = Colors.Yellow;
+            //LblLamp.TextColor = (Color)Resources["Gray600"]; 
+            LblLamp.TextColor = Color.FromRgb(20,20,20);
+        }
+        else {
+            LblLamp.BackgroundColor = Colors.Gray;
+            LblLamp.TextColor =  Colors.White;
+        }
+    
+    }
 	
 }
 
