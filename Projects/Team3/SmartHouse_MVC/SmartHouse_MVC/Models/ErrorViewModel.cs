30c9378a50d83df6c3a4fa4d64341e176b11fa7c
Tetyana-K <tanya.kyryk@gmail.com>
2022-10-02 21:09:42 +0300

    create start project with MVC

---
+++ Projects/Team3/SmartHouse_MVC/SmartHouse_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartHouse_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
