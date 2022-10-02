b070a7e4954d5e03bac4cd5738b874679633d051
Tetyana-K <tanya.kyryk@gmail.com>
2022-10-02 22:43:06 +0300

    create DAL(House and others)

---
+++ Projects/Team3/SmartHouse_MVC/BAL/Models/NeedsRegimeEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Models
+{
+  public  enum NeedsRegime { NeedWarm, NeedCool, NeedOutFire }
+
+   public class NeedsRegimeEventArgs : EventArgs
+    {
+        public NeedsRegime NeedsRegime { get; set; }
+    }
+}
